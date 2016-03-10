e8a67a705436db0626b6daf6f3ea3d722c0465af
hbcui1984 <hbcui1984@163.com>
2016-03-10 21:06:53 +0800

    流应用环境下不执行更新检查

--- examples/hello-mui/js/update.js
+++ examples/hello-mui/js/update.js
@@ -1,15 +1,19 @@
-var server="http://www.dcloud.io/check/update";//获取升级描述文件服务器地址
-	
-function update(){
-	mui.getJSON(server,{"appid":plus.runtime.appid,"version":plus.runtime.version,"imei":plus.device.imei},function (data) {
-		if(data.status){
-			plus.nativeUI.confirm( data.note, function(event){
-				if ( 0==event.index ) {
-					plus.runtime.openURL( data.url );
+var server = "http://www.dcloud.io/check/update"; //获取升级描述文件服务器地址
+
+function update() {
+	mui.getJSON(server, {
+		"appid": plus.runtime.appid,
+		"version": plus.runtime.version,
+		"imei": plus.device.imei
+	}, function(data) {
+		if (data.status) {
+			plus.nativeUI.confirm(data.note, function(event) {
+				if (0 == event.index) {
+					plus.runtime.openURL(data.url);
 				}
-			}, data.title, ["立即更新","取　　消"] );
+			}, data.title, ["立即更新", "取　　消"]);
 		}
 	});
 }
 
-mui.plusReady(update);
+mui.os.plus && !mui.os.stream && mui.plusReady(update);
