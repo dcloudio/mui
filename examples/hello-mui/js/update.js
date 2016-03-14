f7806ecfdf549afe4ac2ca88c626ac213cf41882
hbcui1984 <hbcui1984@163.com>
2016-03-14 20:02:47 +0800

    #检查更新#完善注释，补充服务器返回的数据格式说明

--- examples/hello-mui/js/update.js
+++ examples/hello-mui/js/update.js
@@ -1,3 +1,19 @@
+/**
+ * 参考文档：http://ask.dcloud.net.cn/article/431
+ * 升级文件为JSON格式数据，如下：
+ * 
+ * 需升级
+ {
+	"status":1,
+	"version": "2.6.0",
+	"title": "Hello MUI版本更新",
+	"note": "修复“选项卡+下拉刷新”示例中，某个选项卡滚动到底时，会触发所有选项卡上拉加载事件的bug；\n修复Android4.4.4版本部分手机上，软键盘弹出时影响图片轮播组件，导致自动轮播停止的bug；",
+	"url": "http://www.dcloud.io/hellomui/HelloMUI.apk"
+}
+*
+* 无需升级
+{"status":0}
+ */
 var server = "http://www.dcloud.io/check/update"; //获取升级描述文件服务器地址
 
 function update() {
