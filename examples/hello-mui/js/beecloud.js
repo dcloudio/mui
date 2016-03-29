ed1d16d6aff0a8c80c2e351e61aeb089e98b6548
hbcui1984 <hbcui1984@163.com>
2016-03-29 11:58:16 +0800

    配置业务支持的支付通道
    逻辑：配置服务端支持哪些支付通道后，再从plus.payment.getChannels中筛选

--- examples/hello-mui/js/beecloud.js
+++ examples/hello-mui/js/beecloud.js
@@ -20,19 +20,19 @@ beecloud.genBillNo = function() {
 };
 
 mui.plusReady(function() {
+	//配置业务支持的支付通道，支付需要服务端支持，在BeeCloud上支持支付宝支付和微信支付；
+	var support_channel = ['alipay', 'wxpay']; 
 	plus.payment.getChannels(function(s) {
 		var oauthArea = document.querySelector('.oauth-area');
 		for (var i = 0; i < s.length; i++) {
 			if(s[i].serviceReady){
-				//流应用情况下，暂不显示微信支付；
-				if(s[i].id == 'wxpay' && mui.os.stream){
-					continue;
+				if(~support_channel.indexOf(s[i].id)){
+					var btn = document.createElement('div');
+					btn.setAttribute('id', s[i].id);
+					btn.className = 'mui-btn mui-btn-blue mui-btn-block pay';
+					btn.innerText = s[i].description+'支付'
+					oauthArea.appendChild(btn);
 				}
-				var btn = document.createElement('div');
-				btn.setAttribute('id', s[i].id);
-				btn.className = 'mui-btn mui-btn-blue mui-btn-block pay';
-				btn.innerText = s[i].description+'支付'
-				oauthArea.appendChild(btn);
 			}
 		}
 		channels = s;
@@ -50,6 +50,10 @@ function getRandomHost() {
 	return "" + hosts[parseInt(3 * Math.random())] + "/2/rest/app/bill";
 }
 
+/**
+ * 获取支付通道
+ * 
+ */
 function getPayChannel(bc_channel) {
 	var dc_channel_id = '';
 	switch (bc_channel) {
@@ -73,7 +77,7 @@ function getPayChannel(bc_channel) {
 
 function doPay(payData, cbsuccess, cberror) {
 	if (w) return; 
- 
+ 	
 	w = plus.nativeUI.showWaiting();
 	mui.ajax(getRandomHost(), {
 		data: JSON.stringify(payData),
@@ -103,7 +107,9 @@ function doPay(payData, cbsuccess, cberror) {
 					}
 					plus.payment.request(payChannel, paySrc, cbsuccess, cberror);
 				} else if (payData.channel == 'UN_WEB') {
+					//银联在线支付
 					var web = plus.webview.create('', "beecloudPay");
+					//注入JS，解决银联界面返回的问题
 					web.setJsFile('_www/js/95516.js');
 					web.addEventListener('loaded', function() {
 						if (!web.isVisible()) {
