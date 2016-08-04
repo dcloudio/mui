52dec44f1ba5f63e0d0999e05546a4c66f389284
hbcui1984 <hbcui1984@163.com>
2016-08-04 19:03:12 +0800

    优化mui.ajax对applicaiton/json请求格式的支持

--- js/pullrefresh.5+.js
+++ js/pullrefresh.5+.js
@@ -4,7 +4,7 @@
  * @returns {undefined}
  */
 (function($, document) {
-	if (!($.os.plus && $.os.android)) { //仅在android的5+版本使用
+	if(!($.os.plus && $.os.android)) { //仅在android的5+版本使用
 		return;
 	}
 	var CLASS_PLUS_PULLREFRESH = $.className('plus-pullrefresh');
@@ -31,10 +31,10 @@
 			window.addEventListener('dragup', self);
 			document.addEventListener("plusscrollbottom", self);
 			self.scrollInterval = window.setInterval(function() {
-				if (self.isScroll && !self.loading) {
-					if (window.pageYOffset + window.innerHeight + 10 >= document.documentElement.scrollHeight) {
+				if(self.isScroll && !self.loading) {
+					if(window.pageYOffset + window.innerHeight + 10 >= document.documentElement.scrollHeight) {
 						self.isScroll = false; //放在这里是因为快速滚动的话，有可能检测时，还没到底，所以只要有滚动，没到底之前一直检测高度变化
-						if (self.bottomPocket) {
+						if(self.bottomPocket) {
 							self.pullupLoading();
 						}
 					}
@@ -43,22 +43,22 @@
 		},
 		_initPulldownRefreshEvent: function() {
 			var self = this;
-			if (self.topPocket && self.options.webviewId) {
+			if(self.topPocket && self.options.webviewId) {
 				$.plusReady(function() {
 					var webview = plus.webview.getWebviewById(self.options.webviewId);
-					if (!webview) {
+					if(!webview) {
 						return;
 					}
 					self.options.webview = webview;
 					var downOptions = self.options.down;
 					var height = downOptions.height;
 					webview.addEventListener("dragBounce", function(e) {
-						if (!self.pulldown) {
+						if(!self.pulldown) {
 							self._initPulldownRefresh();
 						} else {
 							self.pullPocket.classList.add(CLASS_BLOCK);
 						}
-						switch (e.status) {
+						switch(e.status) {
 							case "beforeChangeOffset": //下拉可刷新状态
 								self._setCaption(downOptions.contentdown);
 								break;
@@ -87,7 +87,7 @@
 		},
 		handleEvent: function(e) {
 			var self = this;
-			if (self.stopped) {
+			if(self.stopped) {
 				return;
 			}
 			//5+的plusscrollbottom当页面内容较少时，不触发
@@ -97,7 +97,7 @@
 			//				}
 			//			}
 			self.isScroll = false;
-			if (e.type === 'dragup' || e.type === 'plusscrollbottom') {
+			if(e.type === 'dragup' || e.type === 'plusscrollbottom') {
 				self.isScroll = true;
 				setTimeout(function() {
 					self.isScroll = false;
@@ -109,7 +109,7 @@
 			this.stopped = !!stopped;
 			//TODO 此处需要设置当前webview的bounce为none,目前5+有BUG
 			var webview = plus.webview.currentWebview();
-			if (this.stopped) {
+			if(this.stopped) {
 				webview.setStyle({
 					bounce: 'none'
 				});
@@ -142,12 +142,17 @@
 				});
 			}.bind(this));
 		},
-		//		_pulldownLoading: function() { //该方法是子页面调用的
-		//			var self = this;
-		//			$.plusReady(function() {
-		//				plus.webview.getWebviewById(self.options.webviewId).evalJS("mui&&mui.options.pullRefresh.down&&mui.options.pullRefresh.down.callback()");
-		//			});
-		//		},
+		_pulldownLoading: function() { //该方法是父页面调用的
+			var self = this;
+			$.plusReady(function() {
+				var childWebview = plus.webview.getWebviewById(self.options.webviewId);
+				childWebview.setBounce({
+					offset: {
+						top: self.options.down.height + "px"
+					}
+				});
+			});
+		},
 		endPulldownToRefresh: function() { //该方法是子页面调用的
 			var webview = plus.webview.currentWebview();
 			webview.parent().evalJS("mui&&mui(document.querySelector('.mui-content')).pullRefresh('" + JSON.stringify({
@@ -156,7 +161,7 @@
 		},
 		_endPulldownToRefresh: function() { //该方法是父页面调用的
 			var self = this;
-			if (self.topPocket && self.options.webview) {
+			if(self.topPocket && self.options.webview) {
 				self.options.webview.endPullToRefresh(); //下拉刷新所在webview回弹
 				self.loading = false;
 				self._setCaption(self.options.down.contentdown, true);
@@ -167,9 +172,9 @@
 		},
 		pullupLoading: function(callback) {
 			var self = this;
-			if (self.isLoading) return;
+			if(self.isLoading) return;
 			self.isLoading = true;
-			if (self.pulldown !== false) {
+			if(self.pulldown !== false) {
 				self._initPullupRefresh();
 			} else {
 				this.pullPocket.classList.add(CLASS_BLOCK);
@@ -186,11 +191,11 @@
 		},
 		endPullupToRefresh: function(finished) {
 			var self = this;
-			if (self.pullLoading) {
+			if(self.pullLoading) {
 				self.pullLoading.classList.remove(CLASS_VISIBILITY);
 				self.pullLoading.classList.add(CLASS_HIDDEN);
 				self.isLoading = false;
-				if (finished) {
+				if(finished) {
 					self.finished = true;
 					self.pullCaption.className = CLASS_PULL_CAPTION + ' ' + CLASS_PULL_CAPTION_NOMORE;
 					self.pullCaption.innerHTML = self.options.up.contentnomore;
@@ -224,8 +229,11 @@
 		scrollTo: function(x, y, time) {
 			$.scrollTo(y, time);
 		},
+		scrollToBottom: function(time) {
+			$.scrollTo(document.documentElement.scrollHeight, time);
+		},
 		refresh: function(isReset) {
-			if (isReset && this.finished) {
+			if(isReset && this.finished) {
 				this.enablePullupToRefresh();
 				this.finished = false;
 			}
@@ -235,23 +243,27 @@
 	//override h5 pullRefresh
 	$.fn.pullRefresh = function(options) {
 		var self;
-		if (this.length === 0) {
+		if(this.length === 0) {
 			self = document.createElement('div');
 			self.className = 'mui-content';
 			document.body.appendChild(self);
 		} else {
 			self = this[0];
 		}
+		var args = options;
 		//一个父需要支持多个子下拉刷新
 		options = options || {}
-		if (typeof options === 'string') {
+		if(typeof options === 'string') {
 			options = $.parseJSON(options);
 		};
 		!options.webviewId && (options.webviewId = (plus.webview.currentWebview().id || plus.webview.currentWebview().getURL()));
 		var pullRefreshApi = null;
 		var attrWebviewId = options.webviewId && options.webviewId.replace(/\//g, "_"); //替换所有"/"
 		var id = self.getAttribute('data-pullrefresh-plus-' + attrWebviewId);
-		if (!id) { //避免重复初始化5+ pullrefresh
+		if(!id && typeof args === 'undefined') {
+			return false;
+		}
+		if(!id) { //避免重复初始化5+ pullrefresh
 			id = ++$.uuid;
 			self.setAttribute('data-pullrefresh-plus-' + attrWebviewId, id);
 			document.body.classList.add(CLASS_PLUS_PULLREFRESH);
@@ -259,9 +271,9 @@
 		} else {
 			pullRefreshApi = $.data[id];
 		}
-		if (options.down && options.down.auto) { //如果设置了auto，则自动下拉一次
-			pullRefreshApi.pulldownLoading(); //parent webview
-		} else if (options.up && options.up.auto) { //如果设置了auto，则自动上拉一次
+		if(options.down && options.down.auto) { //如果设置了auto，则自动下拉一次
+			pullRefreshApi._pulldownLoading(); //parent webview
+		} else if(options.up && options.up.auto) { //如果设置了auto，则自动上拉一次
 			pullRefreshApi.pullupLoading();
 		}
 		return pullRefreshApi;
