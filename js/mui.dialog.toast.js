f4ca4d8a6f059c06d653e65036e9ceac382dea19
hbcui1984 <hbcui1984@163.com>
2016-11-17 18:07:01 +0800

    div模式的toast改造
    1、改造UI为黑色背景，类似Material UI
    2、支持在plus环境下，强制使用DIV模式的toast
    3、支持设置toast的显示时间

--- js/mui.dialog.toast.js
+++ js/mui.dialog.toast.js
@@ -3,29 +3,55 @@
 	/**
 	 * 自动消失提示框
 	 */
-	$.toast = function(message) {
-		if ($.os.plus) {
+	$.toast = function(message,options) {
+		var durations = {
+		    'long': 3500,
+		    'short': 2000
+		};
+
+		//计算显示时间
+		 options = $.extend({
+	        duration: 'short'
+	    }, options || {});
+
+
+		if ($.os.plus && options.type !== 'div') {
 			//默认显示在底部；
 			$.plusReady(function() {
 				plus.nativeUI.toast(message, {
-					verticalAlign: 'bottom'
+					verticalAlign: 'bottom',
+					duration:options.duration
 				});
 			});
 		} else {
+			if (typeof options.duration === 'number') {
+		        duration = options.duration>0 ? options.duration:durations['short'];
+		    } else {
+		        duration = durations[options.duration];
+		    }
+		    if (!duration) {
+		        duration = durations['short'];
+		    }
 			var toast = document.createElement('div');
 			toast.classList.add($.className('toast-container'));
 			toast.innerHTML = '<div class="' + $.className('toast-message') + '">' + message + '</div>';
 			toast.addEventListener('webkitTransitionEnd', function() {
 				if (!toast.classList.contains(CLASS_ACTIVE)) {
 					toast.parentNode.removeChild(toast);
+					toast = null;
 				}
 			});
+			//点击则自动消失
+			toast.addEventListener('click', function() {
+		        toast.parentNode.removeChild(toast);
+		        toast = null;
+		    });
 			document.body.appendChild(toast);
 			toast.offsetHeight;
 			toast.classList.add(CLASS_ACTIVE);
 			setTimeout(function() {
-				toast.classList.remove(CLASS_ACTIVE);
-			}, 2000);
+				toast && toast.classList.remove(CLASS_ACTIVE);
+			}, duration);
 		}
 	};
 
