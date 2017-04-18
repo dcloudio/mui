9b49d986b86e4b0a48d1a99f286b44371e66d176
nearwmy <1137650620@qq.com>
2017-04-18 14:32:39 +0800

    修复iphone上广告模块未新开广告窗口bug

--- examples/hello-mui/js/ad.js
+++ examples/hello-mui/js/ad.js
@@ -1,15 +1,25 @@
-document.querySelector("body").addEventListener('click',function (event) {
-	event.preventDefault();
-	event.stopPropagation();
-	var target = event.target;
-	for (; target && target !== document; target = target.parentNode) {
-		if(target.tagName=='A'){
-			plus.runtime.openURL(target.getAttribute("href"));
-			break;
+document.addEventListener('DOMContentLoaded',function(){
+	document.querySelector("body").addEventListener('click',function (event) {
+		event.preventDefault();
+		event.stopPropagation();
+		var target = event.target;
+		for (; target && target !== document; target = target.parentNode) {
+			if(target.tagName=='A'){
+				plus.runtime.openURL(target.getAttribute("href"));
+				break;
+			}
+			console.log("target:"+target.tagName);
 		}
-		console.log("target:"+target.tagName);
-	}
-},true);
-	
+	},true);
+		
+	(function appendVW() {
+		var content = 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no';
+		if(!document.querySelector('meta[name=viewport]')) {
+			var meta = document.createElement('meta');
+			meta.name = 'viewport';
+			meta.content = content;
+			document.getElementsByTagName('head')[0].appendChild(meta);
+		}
+	})();
 
-
+},false)
