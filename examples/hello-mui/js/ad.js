94f36684d3014b66ca8e7b501ebd3c25b0b63214
崔红保 <hbcui1984@163.com>
2017-04-18 18:05:57 +0800

    Update ad.js

--- examples/hello-mui/js/ad.js
+++ examples/hello-mui/js/ad.js
@@ -11,15 +11,14 @@ document.addEventListener('DOMContentLoaded',function(){
 			console.log("target:"+target.tagName);
 		}
 	},true);
-		
-	(function appendVW() {
-		var content = 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no';
-		if(!document.querySelector('meta[name=viewport]')) {
-			var meta = document.createElement('meta');
-			meta.name = 'viewport';
-			meta.content = content;
-			document.getElementsByTagName('head')[0].appendChild(meta);
-		}
-	})();
+	
+	//判断是否包含viewport meta信息，若无，则动态插入
+	var content = 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no';
+	if(!document.querySelector('meta[name=viewport]')) {
+		var meta = document.createElement('meta');
+		meta.name = 'viewport';
+		meta.content = content;
+		document.getElementsByTagName('head')[0].appendChild(meta);
+	}
 
 },false)
