b398e9cbc778c9ea4e9227b6e8ae4f25b15c4527
hbcui1984 <hbcui1984@163.com>
2017-10-19 20:17:33 +0800

    优化原生选项卡实现方案
    通过NView的tags实现，减少view数量

--- examples/nativeTab/js/util.js
+++ examples/nativeTab/js/util.js
@@ -1,8 +1,8 @@
 var util = {
 	options: {
-		active_color:'#007aff',
-		normal_color:'#000',
-		subpages:['html/tab-webview-subpage-chat.html', 'html/tab-webview-subpage-contact.html']
+		ACTIVE_COLOR: "#007aff",
+		NORMAL_COLOR: "#000",
+		subpages: ["html/tab-webview-subpage-chat.html", "html/tab-webview-subpage-contact.html"]
 	},
 	/**
 	 *  简单封装了绘制原生view控件的方法
@@ -15,22 +15,34 @@ var util = {
 	/**
 	 * 初始化首个tab窗口 和 创建子webview窗口 
 	 */
-	initSubpage: function() {
+	initSubpage: function(aniShow) {
 		var subpage_style = {
-				top: '0px',
-				bottom: '51px'
+				top: 0,
+				bottom: 51
 			},
 			subpages = util.options.subpages,
 			self = plus.webview.currentWebview(),
 			temp = {};
-			// 初始化第一个tab项为首次显示
-			temp[self.id] = "true";
-			mui.extend(aniShow, temp);
-			// 初始化绘制首个tab按钮
-			util.toggleNview(self.getStyle().subNViews[0], 0);
 			
+		//兼容安卓上添加titleNView 和 设置沉浸式模式会遮盖子webview内容
+		if(mui.os.android) {
+			if(plus.navigator.isImmersedStatusbar()) {
+				subpage_style.top += plus.navigator.getStatusbarHeight();
+			}
+			if(self.getTitleNView()) {
+				subpage_style.top += 40;
+			}
+			
+		}
+
+		// 初始化第一个tab项为首次显示
+		temp[self.id] = "true";
+		mui.extend(aniShow, temp);
+		// 初始化绘制首个tab按钮
+		util.toggleNview(0);
+
 		for(var i = 0, len = subpages.length; i < len; i++) {
-			
+
 			if(!plus.webview.getWebviewById(subpages[i])) {
 				var sub = plus.webview.create(subpages[i], subpages[i], subpage_style);
 				//初始化隐藏
@@ -43,7 +55,7 @@ var util = {
 	/**	
 	 * 点击切换tab窗口 
 	 */
-	changeSubpage: function(targetPage, activePage) {
+	changeSubpage: function(targetPage, activePage, aniShow) {
 		//若为iOS平台或非首次显示，则直接显示
 		if(mui.os.ios || aniShow[targetPage]) {
 			plus.webview.show(targetPage);
@@ -55,39 +67,22 @@ var util = {
 			plus.webview.show(targetPage, "fade-in", 300);
 		}
 		//隐藏当前 除了第一个父窗口
-		if(activePage !== plus.webview.getLaunchWebview()){
-			plus.webview.hide(activePage);			
+		if(activePage !== plus.webview.getLaunchWebview()) {
+			plus.webview.hide(activePage);
 		}
 	},
 	/**
 	 * 点击重绘底部tab （view控件）
 	 */
-	toggleNview: function(currObj, currIndex) {
-		var self = plus.webview.currentWebview();
-		// 重绘当前nview
-		self.updateSubNViews([{
-			id: currObj.id,
-			tags: [{
-				tag: 'font', //currObj.tag
-				id: currObj.id + 1,
-				position: currObj.tags[0].position,
-				text: currObj.tags[0].text,
-				textStyles: util.changeColor(currObj.tags[0].textStyles, util.options.active_color)
-			}, {
-				tag: 'font',
-				id: currObj.id + 2,
-				position: currObj.tags[1].position,
-				text: currObj.tags[1].text,
-				textStyles: util.changeColor(currObj.tags[1].textStyles, util.options.active_color)
-			}]
-		}])
-
-		// 重绘兄弟nview
-		for(var i = 0; i < 4; i++) {
-			var viewObj = self.getStyle().subNViews[i];
-
-			if(i !== currIndex) {
-				util.updateSubNView(viewObj);
+	toggleNview: function(currIndex) {
+		currIndex = currIndex * 2;
+		// 重绘当前tag 包括icon和text，所以执行两个重绘操作
+		util.updateSubNView(currIndex, util.options.ACTIVE_COLOR);
+		util.updateSubNView(currIndex + 1, util.options.ACTIVE_COLOR);
+		// 重绘兄弟tag 反之排除当前点击的icon和text
+		for(var i = 0; i < 8; i++) {
+			if(i !== currIndex && i !== currIndex + 1) {
+				util.updateSubNView(i, util.options.NORMAL_COLOR);
 			}
 		}
 	},
@@ -96,28 +91,17 @@ var util = {
 	 */
 	changeColor: function(obj, color) {
 		obj.color = color;
-		return obj
+		return obj;
 	},
 	/*
-	 * 利用 webview 提供的 updateSubNViews 方法更新 view 控件
+	 * 利用 plus.nativeObj.View 提供的 drawText 方法更新 view 控件
 	 */
-	updateSubNView: function(obj) {
-		var self = plus.webview.currentWebview();
-		self.updateSubNViews([{
-			id: obj.id,
-			tags: [{
-				tag: 'font', //此处直接赋值因为已知需要更新的tag 类型，不是固定值，请注意
-				id: obj.id + 1,
-				position: obj.tags[0].position,
-				text: obj.tags[0].text,
-				textStyles: util.changeColor(obj.tags[0].textStyles, util.options.normal_color)
-			}, {
-				tag: 'font',
-				id: obj.id + 2,
-				position: obj.tags[1].position,
-				text: obj.tags[1].text,
-				textStyles: util.changeColor(obj.tags[1].textStyles, util.options.normal_color)
-			}]
-		}]);
+	updateSubNView: function(currIndex, color) {
+		var self = plus.webview.currentWebview(),
+			nviewEvent = plus.nativeObj.View.getViewById("tabBar"), // 获取nview控件对象
+			nviewObj = self.getStyle().subNViews[0], // 获取nview对象的属性
+			currTag = nviewObj.tags[currIndex]; // 获取当前需重绘的tag
+
+		nviewEvent.drawText(currTag.text, currTag.position, util.changeColor(currTag.textStyles, color), currTag.id);
 	}
 };
