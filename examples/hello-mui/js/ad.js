93f8c8f56139e51d588f9dd7f889325b301dce53
hbcui1984 <hbcui1984@163.com>
2017-12-09 19:33:05 +0800

    Fix：Hello MUI底部广告位显示布局错乱的bug

--- examples/hello-mui/js/ad.js
+++ examples/hello-mui/js/ad.js
@@ -1,25 +1,33 @@
-document.addEventListener('DOMContentLoaded',function(){
-	document.querySelector("body").addEventListener('click',function (event) {
+(function() {
+	window.addEventListener('click', function(event) {
 		event.preventDefault();
 		event.stopPropagation();
 		var target = event.target;
-		for (; target && target !== document; target = target.parentNode) {
-			if(target.tagName=='A'){
+		for(; target && target !== document; target = target.parentNode) {
+			if(target.tagName == 'A' && target.getAttribute("href")) {
 				plus.runtime.openURL(target.getAttribute("href"));
 				break;
 			}
-			console.log("target:"+target.tagName);
 		}
-	},true);
-	
+	}, true);
+
 	//判断是否包含viewport meta信息，若无，则动态插入
-	var content = 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no';
-	if(!document.querySelector('meta[name=viewport]')) {
-		var meta = document.createElement('meta');
-		meta.name = 'viewport';
-		meta.content = content;
-		document.getElementsByTagName('head')[0].appendChild(meta);
+	function appendMeta() {
+		var content = 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no';
+		if(!document.querySelector('meta[name=viewport]')) {
+			var meta = document.createElement('meta');
+			meta.name = 'viewport';
+			meta.content = content;
+			document.getElementsByTagName('head')[0].appendChild(meta);
+		}
 	}
 
-},false)
-
+	var readyRE = /complete|loaded|interactive/;
+	if(readyRE.test(document.readyState)) {
+		appendMeta();
+	} else {
+		document.addEventListener('DOMContentLoaded', function() {
+			appendMeta();
+		}, false);
+	}
+})();
