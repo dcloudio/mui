519a9cf6c7454e4d6f3eb18a0f10eaa846122856
hbcui1984 <hbcui1984@163.com>
2020-07-24 16:54:52 +0800

    Update update.js

--- examples/hello-mui/js/update.js
+++ examples/hello-mui/js/update.js
@@ -39,4 +39,7 @@ function update() {
 	});
 }
 
-mui.os.plus && !mui.os.stream && mui.plusReady(update);
+// 真机运行不需要检查更新，真机运行时appid固定为'HBuilder'，这是调试基座的appid
+if(plus.runtime.appid !== 'HBuilder' && mui.os.plus && !mui.os.stream ){
+  mui.plusReady(update);
+} 
