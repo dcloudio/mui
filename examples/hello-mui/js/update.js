de0b90e4c92911738a8bd3d5f10de819d6d57f50
hbcui1984 <hbcui1984@163.com>
2020-07-24 16:19:26 +0800

    检查更新服务端地址指向uniCloud云函数

--- examples/hello-mui/js/update.js
+++ examples/hello-mui/js/update.js
@@ -1,10 +1,12 @@
 /**
- * 参考文档：http://ask.dcloud.net.cn/article/431
- * 升级文件为JSON格式数据，如下：
+ * 5+ App客户端升级参考：http://ask.dcloud.net.cn/article/431
+ * 服务端升级逻辑参考：https://ext.dcloud.net.cn/plugin?id=2226
+ * 
+ * 检查升级返回值为JSON格式，如下：
  * 
  * 需升级
  {
-	"status":1,
+	"isUpdate":true,
 	"version": "2.6.0",
 	"title": "Hello MUI版本更新",
 	"note": "修复“选项卡+下拉刷新”示例中，某个选项卡滚动到底时，会触发所有选项卡上拉加载事件的bug；\n修复Android4.4.4版本部分手机上，软键盘弹出时影响图片轮播组件，导致自动轮播停止的bug；",
@@ -12,9 +14,14 @@
 }
 *
 * 无需升级
-{"status":0}
- */
-var server = "http://www.dcloud.io/check/update"; //获取升级描述文件服务器地址
+{
+  "isUpdate":false
+}
+*
+*/
+
+//服务端检查更新地址，这里是 uniCloud 实现示例，开发者可替换为自己的地址
+var server = "https://7a3e3fa9-7820-41d0-be80-11927ac2026c.bspapp.com/http/update"; 
 
 function update() {
 	mui.getJSON(server, {
@@ -22,7 +29,7 @@ function update() {
 		"version": plus.runtime.version,
 		"imei": plus.device.imei
 	}, function(data) {
-		if (data.status) {
+		if (data.isUpdate) {
 			plus.nativeUI.confirm(data.note, function(event) {
 				if (0 == event.index) {
 					plus.runtime.openURL(data.url);
